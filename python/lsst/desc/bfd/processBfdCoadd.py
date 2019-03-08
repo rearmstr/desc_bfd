ad603f7b31003fa7a4aff51bf28ac6710cfe3061
Armstrong, Robert Ellis <armstrong46@llnl.gov>
2019-03-08 09:39:40 -0800

    clean up coadd processing code

--- python/lsst/desc/bfd/processBfdCoadd.py
+++ python/lsst/desc/bfd/processBfdCoadd.py
@@ -1,111 +1,54 @@
+from lsst.pipe.base import ArgumentParser
+from lsst.pex.config import Config, Field, ConfigurableField
+from lsst.ctrl.pool.parallel import BatchParallelTask, BatchTaskRunner
+from lsst.coadd.utils.coaddDataIdContainer import CoaddDataIdContainer
 
-import lsst.afw.table as afwTable
-import lsst.afw.image as afwImage
-from lsst.pipe.base import Struct, ArgumentParser
-from lsst.ctrl.pool.parallel import BatchPoolTask
-from lsst.pipe.drivers.coaddDriver import CoaddDriverTaskRunner
-from lsst.pipe.tasks.coaddBase import CoaddTaskRunner
-from lsst.pipe.drivers.utils import getDataRef, TractDataIdContainer
-from lsst.ctrl.pool.pool import Pool, abortOnError, NODE
-from lsst.pex.config import Config, Field, ConfigurableField, ListField
-from .measureCoadd import MeasureCoaddTask
-from .processBfdPatch import PatchRunner
-
+from .processBfdPatch import ProcessBfdPatchTask
 
 
 class ProcessBfdCoaddConfig(Config):
-    measure = ConfigurableField(target=MeasureCoaddTask, doc="CCD processing task")
+    processPatch = ConfigurableField(target=ProcessBfdPatchTask, doc="Patch processing task")
     coaddName = Field(dtype=str, default="deep", doc="Name of coadd")
-
-
-class DriverTaskRunner(CoaddTaskRunner):
-
-    def __init__(self, TaskClass, parsedCmd, doReturnResults=False):
-        CoaddTaskRunner.__init__(self, TaskClass, parsedCmd, doReturnResults)
-
-    def makeTask(self, parsedCmd=None, args=None):
-        return self.TaskClass(config=self.config, log=self.log)
-
-    @staticmethod
-    def getTargetList(parsedCmd, **kwargs):
-        """!Get bare butler into Task
-        @param parsedCmd results of parsing command input
-        """
-        kwargs["butler"] = parsedCmd.butler
-        return [(parsedCmd.id.refList, kwargs), ]
 
 
 def unpickle(factory, args, kwargs):
     """Unpickle something by calling a factory"""
     return factory(*args, **kwargs)
 
-class ProcessBfdCoaddTask(BatchPoolTask):
+
+class ProcessBfdCoaddTask(BatchParallelTask):
+    """Process Patches in parallel
+    """
     ConfigClass = ProcessBfdCoaddConfig
     _DefaultName = "processBfdCoadd"
-    RunnerClass = DriverTaskRunner
+    RunnerClass = BatchTaskRunner
 
-    def __init__(self, **kwargs):
-        BatchPoolTask.__init__(self, **kwargs)
+    def __init__(self, butler=None, *args, **kwargs):
+        """!
+        Constructor
+        """
+        BatchParallelTask.__init__(self, *args, **kwargs)
+        self.makeSubtask("processPatch")
 
-        self.makeSubtask('measure')
-
-    def __reduce__(self):
-        """Pickler"""
-        return unpickle, (self.__class__, [], dict(config=self.config, name=self._name,
-                                                   parentTask=self._parentTask, log=self.log,
-                                                   ))
     @classmethod
     def _makeArgumentParser(cls, *args, **kwargs):
         kwargs.pop("doBatch", False)
-        parser = ArgumentParser(name=cls._DefaultName, *args, **kwargs)
+        parser = ArgumentParser(name="processBfdCoadd", *args, **kwargs)
         parser.add_id_argument("--id", "deepCoadd", help="data ID, e.g. --id tract=12345 patch=1,2",
-                               ContainerClass=TractDataIdContainer)
+                               ContainerClass=CoaddDataIdContainer)
         return parser
 
-    @classmethod
-    def batchWallTime(cls, time, parsedCmd, numCores):
-        return time
-#    def run(self, tractPatchRefList, bmeautler, selectIdList=[]):
-#        print "finished"
-#    def runDataRef(self, tractPatchRefList, butler, selectIdList=[]):
-#        """Determine which tracts are non-empty before processing"""
-#        self.log.info('enter run')
-#        pool = Pool("tracts")
-#
-#        return [self.runTract(patchRefList, butler) for patchRefList in tractPatchRefList]
-#
-    @abortOnError
-    def runDataRef(self, tractPatchRefList, butler):
-        """!Determine which tracts are non-empty before processing
-        @param tractPatchRefList: List of tracts and patches to include in the coaddition
-        @param butler: butler reference object
-        @param selectIdList: List of data Ids (i.e. visit, ccd) to consider when making the coadd
-        @return list of references to sel.runTract function evaluation for each tractPatchRefList member
+    def run(self, patchRef):
+        """Process a patch, with scatter-gather-scatter using MPI.
         """
-        pool = Pool("tracts")
-        pool.storeSet(butler=butler, skymap=butler.get(self.config.coaddName + "Coadd_skyMap"))
-
-        return [self.runTract(patchRefList, butler) for patchRefList in tractPatchRefList]
-
-    def runTract(self, patchRefList, butler):
-
-        pool = Pool("patches")
-        pool.map(self.runBfd, patchRefList)
-
-    def runBfd(self, cache, patchRef):
-
-        with self.logOperation("processing %s " % (patchRef.dataId)):
-            try:
-                result = self.measure.run(patchRef)
-            except Exception as e:
-                self.log.warn("Failed to process %s: %s\n" % (patchRef.dataId, e))
-                import traceback
-                traceback.print_exc()
-                return None
+        with self.logOperation("processing %s" % (patchRef.dataId,)):
+            self.processPatch.run(patchRef)
 
     def _getConfigName(self):
         return None
+
     def _getEupsVersionsName(self):
         return None
+
     def _getMetadataName(self):
         return None
--- python/lsst/desc/bfd/processBfdPatch.py
+++ python/lsst/desc/bfd/processBfdPatch.py
@@ -1,60 +1,20 @@
-import sys
-import math
-import collections
+import lsst.pipe.base as pipeBase
+from lsst.pipe.base import ArgumentParser
+from lsst.pex.config import Config, Field, ConfigurableField
+from lsst.pipe.tasks.coaddBase import CoaddDataIdContainer
 
-import lsst.daf.base as dafBase
-import lsst.afw.table as afwTable
-import lsst.afw.image as afwImage
-import lsst.afw.cameraGeom as afwCg
-from lsst.pipe.base import Struct, ArgumentParser
-from lsst.pex.config import Config, Field, ConfigurableField, ListField
-#from hsc.pipe.base.pool import abortOnError, NODE, Pool, Debugger
-#from hsc.pipe.base.parallel import BatchPoolTask
-from lsst.pipe.tasks.coaddBase import CoaddDataIdContainer, SelectDataIdContainer, CoaddTaskRunner
-import lsst.pipe.base
-from .measureCoadd import MeasureCoaddTask
+from .measureMCShear import MeasureMCShearTask
 
-
-from lsst.pipe.drivers.utils import TractDataIdContainer
-
-
-#Debugger().enabled = True
-
-
-class PatchRunner(CoaddTaskRunner):
-
-    def __init__(self, TaskClass, parsedCmd, doReturnResults=False):
-        CoaddTaskRunner.__init__(self, TaskClass, parsedCmd, doReturnResults)
-
-    #def makeTask(self, parsedCmd=None, args=None):
-    #    return self.TaskClass(schema=None, config=self.config, log=self.log)
-
-    @staticmethod
-    def getTargetList(parsedCmd, **kwargs):
-        """!Get bare butler into Task
-        @param parsedCmd results of parsing command input
-        """
-        kwargs["butler"] = parsedCmd.butler
-        return [(parsedCmd.id.refList, kwargs), ]
-
-# class PatchRunner(CoaddTaskRunner):
-#     @staticmethod
-#     def getTargetList(parsedCmd, **kwargs):
-#         """Get bare butler into Task"""
-#         kwargs["butler"] = parsedCmd.butler
-#         return [(parsedCmd.id.refList, kwargs),]
-# 
 
 class ProcessBfdPatchConfig(Config):
-    measure = ConfigurableField(target=MeasureCoaddTask, doc="Patch Processing")
+    measure = ConfigurableField(target=MeasureMCShearTask, doc="Patch Processing")
     coaddName = Field(dtype=str, default="deep", doc="Name of coadd")
 
 
-class ProcessBfdPatchTask(lsst.pipe.base.CmdLineTask):
+class ProcessBfdPatchTask(pipeBase.CmdLineTask):
     """Process an tract at once.
     """
 
-    RunnerClass = PatchRunner
     ConfigClass = ProcessBfdPatchConfig
     _DefaultName = "processBfdPatch"
 
@@ -70,40 +30,25 @@ class ProcessBfdPatchTask(lsst.pipe.base.CmdLineTask):
     def _makeArgumentParser(cls, *args, **kwargs):
         parser = ArgumentParser(name="processBfdPatch", *args, **kwargs)
         parser.add_id_argument("--id", "deepCoadd", help="data ID, e.g. --id tract=12345 patch=1,2",
-                               ContainerClass=TractDataIdContainer)
+                               ContainerClass=CoaddDataIdContainer)
         return parser
 
-    def run(self, tractPatchRefList, butler):
-        pass
-    #@abortOnError
-    def runDataRef(self, tractPatchRefList, butler):
+    def run(self, patchRef):
         """
         """
-        print ('running prep')
         try:
             self.measure.prep()
         except Exception as e:
-            print (e)
+            self.log.warn("Failed to prepare %s: %s\n" % (patchRef.dataId, e))
             return
-        tractIdList = []
-        for tract in tractPatchRefList:
-            for patchRef in tract:
-                result = self.process(patchRef)
 
-
-    def process(self, dataRef):
-        """Process a single Patch
-        """
         try:
-            result = self.measure.run(dataRef)
+            self.measure.run(patchRef)
         except Exception as e:
-            self.log.warn("Failed to process %s: %s\n" % (dataRef.dataId, e))
+            self.log.warn("Failed to process %s: %s\n" % (patchRef.dataId, e))
             import traceback
             traceback.print_exc()
-            return None
-
-        return result
-
+            return
 
     def write(self, cache, struct, focusMd=None):
         """Write the outputs.
@@ -111,7 +56,9 @@ class ProcessBfdPatchTask(lsst.pipe.base.CmdLineTask):
 
     def _getConfigName(self):
         return None
+
     def _getEupsVersionsName(self):
         return None
+        
     def _getMetadataName(self):
         return None
